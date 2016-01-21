7b1c1c57572d142f7bc633c37d48599191c97aa3
Edj3 <ejmann@udel.edu>
2016-01-21 01:25:56 -0500

    Challenge Complete.
    Images Download Appropriately, RecyclerView fully functional

--- app/src/main/java/com/mannmade/possible_mannej/MainActivity.java
+++ app/src/main/java/com/mannmade/possible_mannej/MainActivity.java
@@ -1,5 +1,7 @@
 package com.mannmade.possible_mannej;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +10,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
@@ -37,44 +43,6 @@ public class MainActivity extends AppCompatActivity {
         new DownloadJSONTask().execute();
     }
 
-    //AsyncTask to download list
-    private class DownloadJSONTask extends AsyncTask<Void, Void, Boolean> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            jParser = new JSONParser();
-        }
-
-        protected Boolean doInBackground(Void... Void) {
-            try{
-                //download all images into the list that holds
-                //if (peopleList == null || peopleList.isEmpty()){
-                    //Borrowed raw URL from github link
-                    //initialize
-                    json =  ConnectionManager.getInstance().getConnectionToURL("http://de-coding-test.s3.amazonaws.com/books.json");
-                    //Log.v("Downloading from URL", "JSON Object = " + json);
-                    //Log.v("Count of items", String.valueOf(jParser.getJSONforString(json).size()));
-                //}
-                return false;
-            }catch(Exception e){
-                e.printStackTrace();
-                return true;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Boolean isError) {
-            super.onPostExecute(isError);
-            System.out.println(json);
-            if (!isError) {
-                // specify an adapter (see also next example)
-                mAdapter = new MyAdapter(jParser.getJSONforString(json));
-                mRecyclerView.setAdapter(mAdapter);
-            }
-        }
-    }
-
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         private ArrayList<LinkedHashMap<String, String>> mDataset;
 
@@ -85,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
             // each data item is just a string in this case
             protected TextView titleText;
             protected TextView authorText;
-            protected TextView ImageText;
+            public ImageView imageText;
 
             public ViewHolder(View v) {
                 super(v);
-                titleText =  (TextView) v.findViewById(R.id.item_title);
-                authorText = (TextView)  v.findViewById(R.id.item_author);
-                ImageText = (TextView)  v.findViewById(R.id.item_image);
+                imageText = (ImageView) v.findViewById(R.id.item_image);
+                titleText = (TextView) v.findViewById(R.id.item_title);
+                authorText = (TextView) v.findViewById(R.id.item_author);
             }
         }
 
@@ -105,8 +73,7 @@ public class MainActivity extends AppCompatActivity {
         public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             // create a new view
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ebay_list_item, parent, false);
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
+            return new ViewHolder(v);
         }
 
         // Replace the contents of a view (invoked by the layout manager)
@@ -115,13 +82,80 @@ public class MainActivity extends AppCompatActivity {
             // - get element from your dataset at this position
             // - replace the contents of the view with that element
             holder.titleText.setText(mDataset.get(position).get("title"));
+            holder.authorText.setText(mDataset.get(position).get("author"));
 
+            new DownloadImageTask(holder.imageText, mDataset.get(position).get("imageURL")).execute();
         }
 
         // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
             return mDataset.size();
+        }
+    }
+
+    //AsyncTask to download list
+    private class DownloadJSONTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            jParser = new JSONParser();
+        }
+
+        protected Boolean doInBackground(Void... Void) {
+            try{
+                //initialize
+                json =  ConnectionManager.getInstance().getConnectionToURL("http://de-coding-test.s3.amazonaws.com/books.json");
+                return false;
+            }catch(Exception e){
+                e.printStackTrace();
+                return true;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isError) {
+            super.onPostExecute(isError);
+            //System.out.println(json);
+            if (!isError) {
+                // specify an adapter (see also next example)
+                mAdapter = new MyAdapter(jParser.getJSONforString(json));
+                mRecyclerView.setAdapter(mAdapter);
+            }
+        }
+    }
+
+    public class DownloadImageTask extends AsyncTask<URL, Void, Bitmap> {
+        private final WeakReference<ImageView> containerImageView;
+        String URL;
+
+        public DownloadImageTask(ImageView imageView, String url) {
+            this.containerImageView = new WeakReference<>(imageView);
+            this.URL = url;
+        }
+
+        @Override
+        protected Bitmap doInBackground(URL... params) {
+            //URL imageURL = params[0];
+            Bitmap downloadedBitmap = null;
+            try {
+                URL imageURL = new URL(this.URL);
+                InputStream inputStream = imageURL.openStream();
+                downloadedBitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return downloadedBitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            ImageView imageView = this.containerImageView.get();
+
+            if (imageView != null && result != null) {
+                imageView.setImageBitmap(result);
+            }
         }
     }
 }
